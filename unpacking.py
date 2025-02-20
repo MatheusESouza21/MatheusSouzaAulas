4d90719a22071e5200ecb7638a6e90ae8d2a2c9b
MatheusESouza21 <bmakgacha@gmail.com>
2025-02-20 18:54:29 -0300

    Create unpacking.py

---
+++ unpacking.py
@@ -0,0 +1,7 @@
+vendas = ('Camille','14/02/2025','28/10/2008',500,'Estagiario')
+nome, data_contratação, data_nacimento, salario, cargo = vendas
+print(nome)
+print(data_contratação)
+print(data_nacimento)
+print(salario)
+print(cargo)
